b45e84761b5fbc8f7fd7b22a46d265706e25265c
Mihir Pandya <mihir.m.pandya@gmail.com>
2014-08-16 17:29:46 +0800

    Cleaned up a little

--- app/routes.js
+++
@@ -1,9 +0,0 @@
-
-module.exports = function(app) {
-
-  app.get('*', function(req, res) {
-    // load the single view file
-    // angular will handle the page changes on the front-end
-    res.sendfile('./public/index.html');
-  });
-};
