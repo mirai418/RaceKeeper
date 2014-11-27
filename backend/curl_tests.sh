8809b70268bbdc9df0a7746db0c86bd6b5c4532c
Mirai Akagawa <mirai418@me.com>
2014-11-27 03:20:22 -0500

    damn son

--- backend/curl_tests.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-curl --data '{"member_id":1234, "race_id":"4321"}' 127.0.0.1:8080/addMemberToRaceGroup/
-curl --data '{"start_date": "Aug 12", "end_date": "Aug 14", "activity_type": "Running", "race_name": "Summer Race"}' 127.0.0.1:8080/addRaceGroup/
